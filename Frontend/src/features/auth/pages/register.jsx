e9cc2992508789a4518e464945861ccda9d353a1
opxshivam45 <shivamsharmaff1@gmail.com>
2026-07-21 12:11:44 +0530

    mae change in my frontend folder

--- Frontend/src/features/auth/pages/register.jsx
+++ Frontend/src/features/auth/pages/register.jsx
@@ -1,7 +1,36 @@
 import react from "react";
+import {useNavigate,Link} from 'react-router'
 
 const Register = () => {
-  return <div>Register Page</div>;
+
+  const navigate = useNavigate()
+  const handleSubmit = (e) =>{
+    e.prevenDefault()
+  }
+
+  return (
+    <main>
+      <div className='form-container'>
+        <h1>Register</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="input-group">
+            <label htmlFor='username'>Username</label>
+            <input type="text" id="username" name='username' placeholder='Enter username'/>
+          </div>
+          <div className="input-group">
+            <label htmlFor='email'>Email</label>
+            <input type="email" id="email" name='email' placeholder='Enter email address'/>
+          </div>
+          <div className="input-group">
+            <label htmlFor='password'>password</label>
+            <input type="password" id="password" name='password' placeholder='Enter password '/>
+          </div>
+          <button className='button primary-button'>Register</button>
+        </form>
+        <p>Already have an Account ? <Link to="/login">Login</Link></p>
+      </div>
+    </main>
+  )
 };
 
 export default Register;
