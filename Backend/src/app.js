d6c6f4e8e28e6c4fd01d5b6c42ff4e6290bafcfc
opxshivam45 <shivamsharmaff1@gmail.com>
2026-09-19 23:22:55 +0530

    changes on backend

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,19 +1,69 @@
-const express = require("express");
+// const express = require("express");
+// const cookieParser = require("cookie-parser")
+// const cors = require("cors")
+// const app = express();
+
+// app.use(express.json())
+// app.use(cookieParser())
+// app.use(cors({
+//     origin:"http://localhost:5173",
+//     credentials:true
+// }))
+// //require all routes here :
+// const authRouter = require("./routes/auth.routes");
+// const interviewRouter = require("./routes/interview.routes")
+// //using all the routes here :
+// app.use("/api/auth",authRouter)
+// app.use("/api/interview", interviewRouter)
+
+// module.exports = app;
+//******------------------ */
+// const express = require("express");
+// const cookieParser = require("cookie-parser");
+// const cors = require("cors");
+
+// const app = express();
+
+// app.use(express.json());
+// app.use(cookieParser());
+
+// app.use(cors({
+//     origin: process.env.FRONTEND_URL || "http://localhost:5173",
+//     credentials: true
+// }));
+
+// // require all routes here
+// const authRouter = require("./routes/auth.routes");
+// const interviewRouter = require("./routes/interview.routes");
+
+// // using all routes here
+// app.use("/api/auth", authRouter);
+// app.use("/api/interview", interviewRouter);
+
+// module.exports = app;
+
+const express = require("express")
 const cookieParser = require("cookie-parser")
 const cors = require("cors")
-const app = express();
+
+const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
+    origin: "http://localhost:5173",
+    credentials: true
 }))
-//require all routes here :
-const authRouter = require("./routes/auth.routes");
+
+/* require all the routes here */
+const authRouter = require("./routes/auth.routes")
 const interviewRouter = require("./routes/interview.routes")
-//using all the routes here :
-app.use("/api/auth",authRouter)
+
+
+/* using all the routes here */
+app.use("/api/auth", authRouter)
 app.use("/api/interview", interviewRouter)
 
-module.exports = app;
+
+
+module.exports = app
